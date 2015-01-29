882b61c27a44c3b8de0e4d219b36d4cba96f93bc
Hein Bekker <hein@netbek.co.za>
2015-01-29 23:51:35 +0200

    Use $document

--- dist/js/nb-scrollbar-width.js
+++ dist/js/nb-scrollbar-width.js
@@ -13,19 +13,25 @@
 		.module('nb.scrollbarWidth', [])
 		.factory('nbScrollbarWidth', nbScrollbarWidth);
 
-	nbScrollbarWidth.$inject = ['$window'];
-	function nbScrollbarWidth ($window) {
-		var body = $window.document.documentElement;
-		var $body = angular.element(body);
-		var $div = angular.element('<div style="width:50px; height:50px; overflow:hidden; position:absolute; top:-200px; left:-200px;"></div>');
-		var $innerDiv = angular.element('<div style="height:200%; min-height:100px;"></div>');
-		$div.append($innerDiv);
-		$body.append($div);
-		var w1 = $innerDiv[0].offsetWidth;
-		$div.css('overflow-y', 'scroll');
-		var w2 = $innerDiv[0].offsetWidth;
-		$div.remove();
+	nbScrollbarWidth.$inject = ['$document'];
+	function nbScrollbarWidth ($document) {
+		var value = 0;
+		var document = $document[0] || {};
+		var body = document.body;
 
-		return w1 - w2;
+		if (body) {
+			var $body = angular.element(body);
+			var $div = angular.element('<div style="width:50px; height:50px; overflow:hidden; position:absolute; top:-200px; left:-200px;"></div>');
+			var $innerDiv = angular.element('<div style="height:200%; min-height:100px;"></div>');
+			$div.append($innerDiv);
+			$body.append($div);
+			var w1 = $innerDiv[0].offsetWidth;
+			$div.css('overflow-y', 'scroll');
+			var w2 = $innerDiv[0].offsetWidth;
+			$div.remove();
+			value = w1 - w2;
+		}
+
+		return value;
 	}
 })(window, window.angular);
